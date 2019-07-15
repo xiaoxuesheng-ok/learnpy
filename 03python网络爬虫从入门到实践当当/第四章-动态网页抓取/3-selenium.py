a215290309216c0af6d0136b71b5e02cab74a0b5
xiaoxuesheng-ok <fengguang1993@foxmail.com>
2019-07-15 12:11:20 +0800

    sapce?

--- 03python网络爬虫从入门到实践当当/第四章-动态网页抓取/3-selenium.py
+++ 03python网络爬虫从入门到实践当当/第四章-动态网页抓取/3-selenium.py
@@ -10,6 +10,7 @@ time.sleep(3)
 driver.switch_to.frame(driver.find_element_by_css_selector("iframe[title='livere']"))
 
 comments = driver.find_elements_by_css_selector('div.reply-content')
+
 for eachcomment in comments:
     content = eachcomment.find_element_by_tag_name('p')
     print (content.text)
